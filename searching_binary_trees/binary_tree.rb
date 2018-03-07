e6eff2535f7497681225f4987c9668b7939428d2
Devin Mork <devin.mork@gmail.com>
2018-03-07 06:56:30 -0600

    Refactor #build_tree to begin from root node with each passed value.

--- searching_binary_trees/binary_tree.rb
+++ searching_binary_trees/binary_tree.rb
@@ -5,36 +5,94 @@
 class Node
   attr_accessor :parent, :left_child, :right_child, :value
 
-  def initialize(value)
+  def initialize(value, parent = nil, left_child = nil, right_child = nil)
     # New node initializes with value only. Related nodes are nil.
 
-    @parent = nil
-    @left_child = nil
-    @right_child = nil
+    @parent = parent
+    @left_child = left_child
+    @right_child = right_child
     @value = value
+  end
+
+  def to_s
+    value.to_s
   end
 end
 
-def build_tree(data, parent_node = nil)
+def build_tree(data)
   # Takes in array of values, recursively creates binary tree.
   # Returns first node, which anchors to the rest of the tree.
 
   this_value = data.shift
-  this_node = Node.new(this_value)
-  this_node.parent = parent_node
+  root_node = Node.new(this_value)
 
-  return if data.empty?
+  while data.empty? == false
+    this_value = data.shift
+    add_node(this_value, root_node)
 
-  if this_value <= data[0]
-    this_node.left_child = build_tree(data, this_node)
-  else
-    this_node.right_child = build_tree(data, this_node)
   end
 
-  this_node
+  root_node
 end
 
+def add_node(value, node)
+  # Compares down the binary tree to find best position for
+  # passed value.
+
+  if value <= node.value
+    if node.left_child.nil?
+      node.left_child = Node.new(value, node)
+    else
+      go_to_left_child(value, node)
+    end
+  else
+    go_to_right_child(value, node)
+  end
+end
+
+def go_to_right_child(value, node)
+  if node.right_child.nil?
+    node.right_child = Node.new(value, node)
+  else
+    add_node(value, node.right_child)
+  end
+end
+
+def go_to_left_child(value, node)
+  if value > node.left_child.value
+    bump_left_child(value, node)
+  else
+    add_node(value, node.left_child)
+  end
+end
+
+def bump_left_child(new_value, current_node)
+  # Creates new left_child node using new_value
+  # Assigns relationships to take the place of current_node.left_child
+  # The bumped node keeps all dependencies
+
+  old_left_child = current_node.left_child
+  new_left_child = Node.new(new_value, current_node, old_left_child)
+
+  current_node.left_child = new_left_child
+  old_left_child.parent = new_left_child
+
+  new_left_child
+end
+
+def find_root_node(node)
+  # Climbs up tree to find node that has no parent.
+
+  node = node.parent while node.parent.nil? == false
+  node
+end
+
+def breadth_first_search(binary_tree)
+  # Classice breadth-first-search algorithm
+
+end
 
 
 tree = build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 p tree
+ 
